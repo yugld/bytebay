6cd555d4ed60dc1fe1946ab4e4fdfd44709be1ff
yugld <yulliagladysheva@gmail.com>
2024-05-07 17:57:57 +0300

    add maxwidth wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+import Link from "next/link";
+
+const perks = [
+    {
+        name: "Instant Delivery",
+        Icon: ArrowDownToLine,
+        description:
+            "Get your assets delivered to your email in seconds and download them right away.",
+    },
+    {
+        name: "Guaranteed Quality",
+        Icon: CheckCircle,
+        description:
+            "Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.",
+    },
+    {
+        name: "For the Planet",
+        Icon: Leaf,
+        description:
+            "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
+    },
+];
 
 export default function Home() {
-    return <p>bonjour!</p>;
+    return (
+        <>
+            <MaxWidthWrapper>
+                <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+                    bonjour
+                </div>
+            </MaxWidthWrapper>
+        </>
+    );
 }
