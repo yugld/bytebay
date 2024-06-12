7a38bfc75efc0dd418051d13ce897e9be000cadf
yugld <yulliagladysheva@gmail.com>
2024-06-12 23:40:31 +0300

    del valuta

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'USD' | 'EUR' | 'RUB' | 'GBP' 
+    currency?: 'USD' | 'EUR' | 'RUB'
     notation?: Intl.NumberFormatOptions['notation']
   } = {}
 ) {
