7bb62e658fc0a4e823f031622fb4d58ba9690e64
yugld <yulliagladysheva@gmail.com>
2024-06-17 23:39:05 +0300

    fix server

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -39,8 +39,8 @@ const Cart = () => {
                             <Separator />
                             <div className="space-y-1.5 text-sm">
                                 <div className="flex">
-                                    <span className="flex-1">Бесплатная</span>
-                                    <span>Доставка</span>
+                                    <span className="flex-1">Доставка</span>
+                                    <span>Бесплатная</span>
                                 </div>
                                 <div className="flex">
                                     <span className="flex-1">Комиссия</span>
