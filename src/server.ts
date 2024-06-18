061d8b512366984ed0f31cd88aec2dde122635dc
yugld <yulliagladysheva@gmail.com>
2024-06-18 22:50:24 +0300

    setting up server fix error at monogodb

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,14 @@
 import express from 'express'
 import { getPayloadClient } from './get-payload'
 import { nextApp, nextHandler } from './next-utils'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { appRouter } from './trpc'
-import { inferAsyncReturnType } from '@trpc/server'
 import bodyParser from 'body-parser'
 import { IncomingMessage } from 'http'
-import { stripeWebhookHandler } from './webhooks'
-import nextBuild from 'next/dist/build'
 import path from 'path'
 import { PayloadRequest } from 'payload/types'
 import { parse } from 'url'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
-
-const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({
-  req,
-  res,
-})
-
-export type ExpressContext = inferAsyncReturnType<
-  typeof createContext
->
 
 export type WebhookRequest = IncomingMessage & {
   rawBody: Buffer
@@ -37,12 +20,6 @@ const start = async () => {
       req.rawBody = buffer
     },
   })
-
-  app.post(
-    '/api/webhooks/stripe',
-    webhookMiddleware,
-    stripeWebhookHandler
-  )
 
   const payload = await getPayloadClient({
     initOptions: {
@@ -83,15 +60,6 @@ const start = async () => {
 
     return nextApp.render(req, res, '/cart', query)
   })
-
-  app.use('/cart', cartRouter)
-  app.use(
-    '/api/trpc',
-    trpcExpress.createExpressMiddleware({
-      router: appRouter,
-      createContext,
-    })
-  )
 
   app.use((req, res) => nextHandler(req, res))
 
