3c6fff530c55756d3aea01f4a3e80a457442a73a
yugld <yulliagladysheva@gmail.com>
2024-07-03 23:38:15 +0300

    add products fields

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -9,50 +9,115 @@ export const Products: CollectionConfig = {
     access: {},
     fields: [
         {
-        name: 'user',
-        type: 'relationship',
-        relationTo: 'users',
-        required: true,
-        hasMany: false,
-        admin: {
-            condition: () => false,
-        },
-        },
-        {
-        name: 'name',
-        label: 'Название',
-        type: 'text',
-        required: true,
+            name: 'user',
+            type: 'relationship',
+            relationTo: 'users',
+            required: true,
+            hasMany: false,
+            admin: {
+                condition: () => false,
+            },
         },
         {
-        name: 'description',
-        type: 'textarea',
-        label: 'Описание',
+            name: 'name',
+            label: 'Название',
+            type: 'text',
+            required: true,
         },
         {
-        name: 'price',
-        label: 'Стоимость в USD',
-        min: 0,
-        max: 1000,
-        type: 'number',
-        required: true,
+            name: 'description',
+            type: 'textarea',
+            label: 'Описание',
         },
         {
-        name: 'category',
-        label: 'Категория',
-        type: 'select',
-        options: PRODUCT_CATEGORIES.map(
-            ({ label, value }) => ({ label, value })
-        ),
-        required: true,
+            name: 'price',
+            label: 'Стоимость в USD',
+            min: 0,
+            max: 1000,
+            type: 'number',
+            required: true,
         },
         {
-        name: 'product_files',
-        label: 'Файлы товаров',
-        type: 'relationship',
-        required: true,
-        relationTo: 'product_files',
-        hasMany: false,
+            name: 'category',
+            label: 'Категория',
+            type: 'select',
+            options: PRODUCT_CATEGORIES.map(
+                ({ label, value }) => ({ label, value })
+            ),
+            required: true,
+        },
+        {
+            name: 'product_files',
+            label: 'Файлы товара',
+            type: 'relationship',
+            required: true,
+            relationTo: 'product_files',
+            hasMany: false,
+        },
+        {
+            name: 'approvedForSale',
+            label: 'Статус',
+            type: 'select',
+            defaultValue: 'pending',
+            access: {},
+            options: [
+                {
+                    label: 'На проверке',
+                    value: 'pending',
+                },
+                {
+                    label: 'Одобрен',
+                    value: 'approved',
+                },
+                {
+                    label: 'Отклонен',
+                    value: 'denied',
+                },
+            ],
+        },
+        {
+            name: 'priceId',
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            type: 'text',
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: 'stripeId',
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            type: 'text',
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: 'images',
+            type: 'array',
+            label: 'Изображения товара',
+            minRows: 1,
+            maxRows: 4,
+            required: true,
+            labels: {
+                singular: 'Изображение',
+                plural: 'Изображения',
+            },
+            fields: [
+                {
+                name: 'image',
+                type: 'upload',
+                relationTo: 'media',
+                required: true,
+                },
+            ],
         },
     ],
 }
