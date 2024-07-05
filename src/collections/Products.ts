22c3bb7569978fc03047d8787d5d1998e50cd0a5
yugld <yulliagladysheva@gmail.com>
2024-07-05 23:25:19 +0300

    add media collection

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORIES } from '../../config'
+import { PRODUCT_CATEGORIES } from '../config'
 import { CollectionConfig } from 'payload/types'
 
 export const Products: CollectionConfig = {
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import { Users } from './collections/Users'
 import { Products } from './collections/Products'
 import path from 'path'
 import dotenv from 'dotenv'
+import { Media } from './collections/Media'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -13,7 +14,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users, Products], 
+  collections: [Users, Products, Media], 
   routes: {
     admin: '/sell',
   },
