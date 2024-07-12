9dfb49c0a72e082c332862d60f94db4e9135c01b
yugld <yulliagladysheva@gmail.com>
2024-07-12 17:37:40 +0300

    add protect api routes with middleware

---
+++ src/trpc/payment-router.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import {
+  privateProcedure,
+  router,
+} from './trpc'
+
+export const paymentRouter = router({
+    createSession: privateProcedure
+        .input(z.object({ productIds: z.array(z.string()) }))
+        .mutation(async ({ ctx, input }) => {
+        const { user } = ctx
+        let { productIds } = input
+    }),
+})
